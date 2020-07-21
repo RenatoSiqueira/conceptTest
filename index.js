ad82a54c9e9f7aa2e9fe34774c72d105ef74214e
Renato <renatoelysiqueira@gmail.com>
2020-07-21 20:42:59 -0300

    final Test

--- index.js
+++ index.js
@@ -21,33 +21,14 @@ app
         const token = await getToken()
         res.json(token.data)
     })
-    .get('/api', async (req, res) => {
+    .post('/api', async (req, res) => {
         const { data: { access_token } } = await getToken()
-        // const payload = req.body
-
-        const payload = {
-            "Language": "ENG",
-            "Currency": "USD",
-            "destination": "MCO",
-            "DateFrom": "11/26/2016",
-            "DateTO": "11/29/2016",
-            "Occupancy": {
-                "AdultCount": "1",
-                "ChildCount": "1",
-                "ChildAges": ["10"]
-            }
-        }
-
-        const config = { headers: { Authorization: `Bearer ${access_token}`, 'Content-Type': 'application/json' } }
-        console.log(access_token, payload, config)
-        try {
-            const result = await axiosInstance.post('api/Ticket/Search', payload, config)
-            console.log('Result')
-            res.json(result.data)
-        } catch (err) {
-            console.log('Error')
-            res.json(err.message)
-        }
+        const payload = req.body
+        const config = { headers: { Authorization: `Bearer ${access_token}`, 'Content-Type': 'application/json;charset=UTF-8' } }
+        axiosInstance
+            .post('api/Ticket/Search', payload, config)
+            .then(e => console.log('-------', e))
+            .catch(e => console.log(e.response.data.Message))
     })
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`))
